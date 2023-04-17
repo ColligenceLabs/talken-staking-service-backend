06a5192f9363c9e2c2981b2f0ccbfed7a53194dd
seongjin1026 <seongjin.kim@owdin.network>
2023-04-17 18:54:51 +0900

    feat: transfer rewards

--- src/utils/awskms.js
+++ src/utils/awskms.js
@@ -1,14 +1,15 @@
 const Web3 = require("web3");
 const { KmsProvider } = require("aws-kms-provider");
+const {provider} = require("../config/constants");
 
 module.exports = {
     getKlaytnKmsWeb3: function () {
         const accessKeyId = process.env.ACCESS_KEY_ID;
         const secretAccessKey = process.env.SECRET_ACCESS_KEY;
         const region = process.env.REGION;
-        const provider = new KmsProvider(process.env.KLAYTN_NODE,
+        const kmsProvider = new KmsProvider(provider[parseInt(process.env.TARGET_NETWORK)],
             {region, keyIds: [process.env.KEY_ID], credential: {accessKeyId, secretAccessKey}});
 
-        return new Web3(provider);
+        return new Web3(kmsProvider);
     },
 }
