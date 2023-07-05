09cd9c48284d8f4b10a2c966184a02d131a4b7e4
Peter H. Nahm <hwnahm@gmail.com>
2023-07-05 17:02:22 +0900

    feat: receive Claimed event

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,12 +1,13 @@
 module.exports = {
-    provider: {
-        8217: 'https://public-node-api.klaytnapi.com/v1/cypress',
-        1001: 'https://public-node-api.klaytnapi.com/v1/baobab',
-    },
-    types: {
-        1: 'Stake',
-        2: 'Unstake',
-        3: 'Cancel',
-        4: 'Transfer',
-    }
+  provider: {
+    8217: 'https://public-node-api.klaytnapi.com/v1/cypress',
+    1001: 'https://public-node-api.klaytnapi.com/v1/baobab',
+  },
+  types: {
+    1: 'Stake',
+    2: 'Unstake',
+    3: 'Cancel',
+    4: 'Transfer',
+    5: 'Claim',
+  },
 };
--- src/controller/blockchain/crawler.js
+++ src/controller/blockchain/crawler.js
@@ -21,18 +21,20 @@ const {types} = require('../../config/constants');
 
 const contracts = [
   '0x6AD500960Fee1a1452130851356985bF696549E3', // StKlay
-  // '0x0013E63515fbCe7Ba92cF783c231C4844B97d118', // NodeManager
+  '0x0013E63515fbCe7Ba92cF783c231C4844B97d118', // NodeManager
 ];
 
 async function parseSharesChanged(eventData) {
   try {
     const web3 = getWeb3();
     // StKlay Event : SharesChanged(address,uint256,uint256,uint256,uint8)
-    if (eventData.topics[0] == '0x0e4033ca59159fed9e716efba93cc8fc4e08e122cce662e9449ef210cca29411') {
+    if (
+      eventData.topics[0] == '0x0e4033ca59159fed9e716efba93cc8fc4e08e122cce662e9449ef210cca29411'
+    ) {
       let contractAddress = eventData.address.toLowerCase();
       const data = web3.eth.abi.decodeParameters(
-          ['uint256', 'uint256', 'uint256', 'uint8'],
-          eventData.data,
+        ['uint256', 'uint256', 'uint256', 'uint8'],
+        eventData.data,
       );
 
       let user = web3.eth.abi.decodeParameters(['address'], eventData.topics[1])[0];
@@ -51,21 +53,21 @@ async function parseSharesChanged(eventData) {
       if (lastHistory) {
         if (changeType === '1' || changeType === '3')
           totalstake = BigNumber.from(lastHistory.totalstake ?? '0')
-              .add(BigNumber.from(amount))
-              .toString();
+            .add(BigNumber.from(amount))
+            .toString();
         else if (changeType === '2')
           totalstake = BigNumber.from(lastHistory.totalstake ?? '0')
-              .sub(BigNumber.from(amount))
-              .toString();
+            .sub(BigNumber.from(amount))
+            .toString();
         else if (changeType === '4') {
           if (BigNumber.from(prevShares).gt(BigNumber.from(shares)))
             totalstake = BigNumber.from(lastHistory.totalstake ?? '0')
-                .sub(BigNumber.from(amount))
-                .toString();
+              .sub(BigNumber.from(amount))
+              .toString();
           else
             totalstake = BigNumber.from(lastHistory.totalstake ?? '0')
-                .add(BigNumber.from(amount))
-                .toString();
+              .add(BigNumber.from(amount))
+              .toString();
         }
       } else totalstake = amount;
 
@@ -97,11 +99,13 @@ async function parseRestakedFromManager(eventData) {
   try {
     const web3 = getWeb3();
     // StKlay Event : RestakedFromManager(uint256,uint256,uint256,uint256,uint256)
-    if (eventData.topics[0] == '0x47c355b9d84fb97b1b36daa506b9fc8f856bb70659ec082640a13dc944dee214') {
+    if (
+      eventData.topics[0] == '0x47c355b9d84fb97b1b36daa506b9fc8f856bb70659ec082640a13dc944dee214'
+    ) {
       let contractAddress = eventData.address.toLowerCase();
       const data = web3.eth.abi.decodeParameters(
-          ['uint256', 'uint256', 'uint256', 'uint256', 'uint256'],
-          eventData.data,
+        ['uint256', 'uint256', 'uint256', 'uint256', 'uint256'],
+        eventData.data,
       );
 
       let totalRestaked = data[0];
@@ -117,21 +121,21 @@ async function parseRestakedFromManager(eventData) {
       // console.log('!! reward = ', reward.toString());
 
       console.log(
-          '!! RestakedFromManager : ',
-          totalRestaked,
-          amount,
-          increase,
-          totalStaking,
-          totalShares,
+        '!! RestakedFromManager : ',
+        totalRestaked,
+        amount,
+        increase,
+        totalStaking,
+        totalShares,
       );
 
       try {
         await models.rewards.createRewards(
-            eventData.blockNumber,
-            transactionHash,
-            amount,
-            increase,
-            totalShares,
+          eventData.blockNumber,
+          transactionHash,
+          amount,
+          increase,
+          totalShares,
         );
       } catch (e) {
         console.log('createRewards error', e);
@@ -146,7 +150,9 @@ async function parseTransfer(eventData) {
   try {
     const web3 = getWeb3();
     // StKlay Event : RestakedFromManager(uint256,uint256,uint256,uint256,uint256)
-    if (eventData.topics[0] == '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef') {
+    if (
+      eventData.topics[0] == '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
+    ) {
       let contractAddress = eventData.address.toLowerCase();
       const data = web3.eth.abi.decodeParameters(['uint256'], eventData.data);
 
@@ -156,14 +162,64 @@ async function parseTransfer(eventData) {
       let transactionHash = eventData.transactionHash;
 
       if (
-          from !== '0x0000000000000000000000000000000000000000000000000000000000000000' &&
-          to !== '0x0000000000000000000000000000000000000000000000000000000000000000'
+        from !== '0x0000000000000000000000000000000000000000000000000000000000000000' &&
+        to !== '0x0000000000000000000000000000000000000000000000000000000000000000'
       ) {
         console.log('!! Transfer : ', from, to, value);
 
         // TODO : DB 작업
         // Transfer 이벤트 저장
         // from totalstake 에서 value 빼기, to totalstake 에 더하기
+      }
+    }
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function parseClaimed(eventData) {
+  try {
+    const web3 = getWeb3();
+    // NodeManager Event : Claimed(address,uint256,uint256);
+    if (
+      eventData.topics[0] == '0x987d620f307ff6b94d58743cb7a7509f24071586a77759b77c2d4e29f75a2f9a'
+    ) {
+      let contractAddress = eventData.address.toLowerCase();
+      const data = web3.eth.abi.decodeParameters(['address', 'uint256', 'uint256'], eventData.data);
+
+      let user = data[0];
+      let amount = data[1];
+      let expired = data[2];
+      let changeType = 5; // 1 stake, 2 unstake, 3 cancel, 4 transfer, 5 claim
+      console.log('!! parseClaimed : ', user, amount, changeType);
+
+      let transactionHash = eventData.transactionHash;
+      const lastHistory = await models.histories.findOne({
+        where: {wallet: user},
+        order: [['id', 'desc']],
+      });
+      let totalstake;
+      if (lastHistory) totalstake = BigNumber.from(lastHistory.totalstake ?? '0').toString();
+      else totalstake = amount;
+
+      if (BigNumber.from(totalstake).lt(BigNumber.from('0')))
+        totalstake = BigNumber.from('0').toString();
+
+      try {
+        const history = new models.histories();
+        history.block_number = eventData.blockNumber;
+        history.tx_hash = transactionHash;
+        history.wallet = user;
+        history.prev_shares = lastHistory.shares;
+        history.shares = lastHistory.shares;
+        history.amount = amount;
+        history.type = types[changeType];
+        history.totalstake = totalstake;
+
+        // TODO : Claim 시 expired가 아닌 경우에만 성공이므로 DB에 저장
+        if (BigNumber.from(expired).eq(BigNumber.from('0'))) await history.save();
+      } catch (e) {
+        console.log('saveHistory error:', e);
       }
     }
   } catch (e) {
@@ -178,13 +234,14 @@ exports.getLastEvents = async function (toBlock, chainName) {
     let lastBlock = await models.lastblock.findByNetwork(process.env.TARGET_NETWORK ?? '1001');
     console.log(chainName, 'getLastEvents', lastBlock.blocknumber, toBlock);
 
-    const result = await web3.eth.getPastLogs(
-      {fromBlock: lastBlock.blocknumber, toBlock: toBlock, address: contracts},
-      // {fromBlock: 120214499, toBlock: 120228768, address: contracts},
-    )
-    .catch((e) => {
-      console.log('collection contract getEvents', e);
-    });
+    const result = await web3.eth
+      .getPastLogs(
+        {fromBlock: lastBlock.blocknumber, toBlock: toBlock, address: contracts},
+        // {fromBlock: 120214499, toBlock: 120228768, address: contracts},
+      )
+      .catch((e) => {
+        console.log('collection contract getEvents', e);
+      });
     console.log('=====!!!!!', result);
     if (result) {
       lastBlock.blocknumber = toBlock + 1;
@@ -199,6 +256,7 @@ exports.getLastEvents = async function (toBlock, chainName) {
             await parseSharesChanged(result[i]);
             await parseRestakedFromManager(result[i]);
             await parseTransfer(result[i]);
+            await parseClaimed(result[i]);
           }
         }
       }
